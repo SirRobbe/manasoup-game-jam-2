794937a6e0f32ddd510ee14417d3cf47d93a6c81
Fabian Behrens <behrens-fabian@gmx.de>
2022-12-02 23:55:06 +0100

    Base Player

---
+++ DreamKeeper/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private void Update()
+    {
+        Timer += Time.deltaTime;
+        
+        int horizontal = Convert.ToInt32(Input.GetKey(KeyCode.D)) - Convert.ToInt32(Input.GetKey(KeyCode.A));
+        int vertical = Convert.ToInt32(Input.GetKey(KeyCode.W)) - Convert.ToInt32(Input.GetKey(KeyCode.S));
+
+        var movement = new Vector3(horizontal, vertical, 0);
+        movement.Normalize();
+
+        transform.position += movement * (Time.deltaTime * Speed);
+
+        if(Input.GetMouseButton(0) && Timer > Cooldown)
+        {
+            var projectile = Instantiate(Projectile, transform.position, Quaternion.identity);
+            var worldPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            projectile.Direction = (worldPosition - transform.position).normalized;
+            Timer = 0f;
+        }
+    }
+
+    public float Speed = 5f;
+    public float Cooldown = 0.15f;
+    [HideInInspector] public float Timer = 0f;
+    
+    public Projectile Projectile;
+}
---
+++ DreamKeeper/Assets/Scripts/Projectile.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    private void Update()
+    {
+        LifeTime -= Time.deltaTime;
+        if(LifeTime < 0)
+        {
+            Destroy(gameObject);
+        }
+        
+        var movement = new Vector3(Direction.x, Direction.y) * (Time.deltaTime * Speed);
+        transform.position += movement;
+    }
+
+    public float LifeTime = 1f;
+    public float Speed = 20f;
+    [HideInInspector] public Vector2 Direction;
+}
