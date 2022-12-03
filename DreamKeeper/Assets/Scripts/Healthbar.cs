d92f8c6b050c2ac88e11ead2428baf84f4263b16
Daniel Wildegger <daniel.wildegger@kreative-kraut.de>
2022-12-03 22:23:45 +0100

    Healthbar

---
+++ DreamKeeper/Assets/Scripts/Healthbar.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Healthbar : MonoBehaviour
+{
+    public void Awake()
+    {
+        Slider = GetComponent<Slider>();
+    }
+    
+    public void Update()
+    {
+        Slider.value = Hero.DreamDepth / 100f;
+        Slider.gameObject.transform.Find("Fill Area").Find("Fill").GetComponent<Image>().color = Color.Lerp(Color.red, Color.green, Slider.value);
+    }
+
+    [HideInInspector] public Slider Slider;
+    public Hero Hero;
+}
