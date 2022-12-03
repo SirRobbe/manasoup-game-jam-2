cadf014f13ab59dff9af705956c4e18bc380eef2
Fabian Behrens <behrens-fabian@gmx.de>
2022-12-03 17:32:06 +0100

    EnemySpawner spawns nightmares with different sprites

--- DreamKeeper/Assets/Scripts/EnemyInstantiater.cs
+++ DreamKeeper/Assets/Scripts/EnemyInstantiater.cs
@@ -1,4 +1,3 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -10,7 +9,8 @@ public class EnemyInstantiater : MonoBehaviour
         if (Timer >= SpawnTimer)
         {
             var position = new Vector3(Random.Range(-10.0f, 10.0f),Random.Range(0f, 5f), 0 );
-            var nightmare = Instantiate(Nightmare, position, Quaternion.identity);
+            var nightmare = Instantiate(Nightmare, position, Quaternion.identity).GetComponent<Nightmare>();
+            nightmare.Renderer.sprite = NightmareSprites[Random.Range(0, NightmareSprites.Count)];
             Timer = 0f;
         }
     }
@@ -18,4 +18,6 @@ public class EnemyInstantiater : MonoBehaviour
     public float SpawnTimer = 2f;
     public float Timer = 0f;
     public GameObject Nightmare;
+
+    public List<Sprite> NightmareSprites;
 }
