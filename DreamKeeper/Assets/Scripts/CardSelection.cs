d09e5b554edee778937322904a853e2b25d568ad
Fabian Behrens <behrens-fabian@gmx.de>
2022-12-03 14:08:05 +0100

    CardSelection before game start

---
+++ DreamKeeper/Assets/Scripts/CardSelection.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CardSelection : MonoBehaviour
+{
+    public void SelectCard(GameObject card)
+    {
+        if(SelectedCards.Contains(card))
+        {
+            SelectedCards.Remove(card);
+        }
+        else
+        {
+            SelectedCards.Add(card);
+        }
+        
+        UpdateSelectedCardsText();
+
+        DreamButton.interactable = SelectedCards.Count == CardsToSelect;
+    }
+
+    public void StartGame()
+    {
+        gameObject.SetActive(false);
+    }
+    
+    private void Awake()
+    {
+        DreamButton.interactable = false;
+        UpdateSelectedCardsText();
+    }
+
+    private void UpdateSelectedCardsText()
+    {
+        SelectedCardsText.text = $"Selected Cards {SelectedCards.Count}/{CardsToSelect}";
+    }
+
+    public TextMeshProUGUI SelectedCardsText;
+
+    public Button DreamButton;
+    
+    [HideInInspector] public List<GameObject> SelectedCards = new List<GameObject>();
+    public int CardsToSelect = 2;
+}
